d7ded27deffe1b764052a74cb84a4b21e3c9e46d
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-11-10 20:39:25 +0800

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,9 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+  pkgs.stdenv.mkDerivation {
+    name = "nac3-env";
+    buildInputs = with pkgs; [
+      llvm_10 clang_10 cargo rustc libffi libxml2
+    ];
+  }
