ced7acd87110b64f79b4af8a4b96f0cc4012ec73
Sebastien Bourdeauducq <sb@m-labs.hk>
2022-01-24 11:38:43 +0800

    check_demos: improve output

--- nac3standalone/demo/check_demos.sh
+++ nac3standalone/demo/check_demos.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+count=0
 for demo in src/*.py; do
-    echo "checking $demo..."
+    echo -n "checking $demo... "
     ./interpret_demo.py $demo > interpreted.log
     ./run_demo.sh $demo > run.log
     diff -Nau interpreted.log run.log
+    echo "ok"
+    let "count+=1"
 done
 
-echo "PASSED"
+echo "Ran $count demo checks - PASSED"
