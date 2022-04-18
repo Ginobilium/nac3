42d1aad50734b70d7f05f8cfd89696facb4bf20f
Sebastien Bourdeauducq <sb@m-labs.hk>
2022-04-18 23:58:43 +0800

    flake: add PGO build to Hydra

--- flake.nix
+++ flake.nix
@@ -161,7 +161,7 @@
       };
 
       hydraJobs = {
-        inherit (packages.x86_64-linux) llvm-nac3 nac3artiq;
+        inherit (packages.x86_64-linux) llvm-nac3 nac3artiq nac3artiq-pgo;
         llvm-nac3-msys2 = packages.x86_64-w64-mingw32.llvm-nac3;
         nac3artiq-msys2 = packages.x86_64-w64-mingw32.nac3artiq;
         nac3artiq-msys2-pkg = packages.x86_64-w64-mingw32.nac3artiq-pkg;
